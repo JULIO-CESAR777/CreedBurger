2941ed92b70f14a6d269a3bd20bbdecce4cd2ac2
Julio Cesar- intelamd <juliomedellin007@hotmail.com>
2026-04-13 23:06:56 -0600

    Todo
    Todo

--- Assets/Dardo.cs
+++ Assets/Dardo.cs
@@ -6,15 +6,22 @@ public class Dardo : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         
-        if (other.gameObject.tag == "Cliente")
+        if (other.gameObject.tag == "Client")
         {
+          
             MoveClient cliente = other.gameObject.GetComponentInParent<MoveClient>();
 
             if (cliente != null)
             {
+              
                 cliente.Aturdir(2f); 
+                Destroy(gameObject);
+                return;
             }
+            
+            
         }
+      
         Destroy(gameObject);
     }
 }
